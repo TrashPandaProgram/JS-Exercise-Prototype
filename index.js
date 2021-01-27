6e0012017144f6f79f5e155989c257524e8ba9a9
Felix Rodriguez <fexxrodriguez567@gmail.com>
2021-01-27 18:39:08 -0500

    finished it almost completetly myself

--- index.js
+++ index.js
@@ -39,16 +39,26 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
   }
- 
- 
 
-  
-  
-  
-  
+  Person.prototype.eat = function(edible) {
+    if(this.stomach.length < 10){
+      this.stomach.push(edible)
+    }
+  }
+
+  Person.prototype.poop = function() {
+    return this.stomach.length = 0
+  }
+
+  Person.prototype.toString = function() {
+    return `${this.name}, ${this.age}`
+  }
+
   /*
     TASK 2
       - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
@@ -63,10 +73,16 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+    this.tank = 0;
+    this.odometer = 0;
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
   }
-  
+
+  Car.prototype.fill = function(gallons) {
+    return this.tank = this.tank + gallons;
+  }
   
   /*
     TASK 3
@@ -75,18 +91,29 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+ function Baby(name, age, favoriteToy) {
+   this.name = name;
+   this.age = age;
+   this.favoriteToy = favoriteToy;
   }
- 
-  
+
+  Baby.prototype.play = function() {
+    return `Playing with ${this.favoriteToy}`
+  }
+
+  Baby.prototype.eat = function() {
+  }
+
+  Baby.prototype.poop = function() {
+  }
+
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. It's the global object in non-strict mode. (Simple Function Call)
+    2. Points to the object on which the function is called, what's on the left of the period. (Implicit Binding)
+    3. Explicitly tell Javascript to set "this" to point to a certain value using call, apply or bind. (Explicit Binding)
+    4. Using the "new" keyword constructs a new object, and "this" point it. (New Binding)
   */
   
   
